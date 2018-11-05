929c19e16444171fa1dfe6f5b6415f8d11a4f0c9
Matthew Thach <mtngocthach1202@gmail.com>
2018-11-06 05:44:32 +0700

    update page 'Thanh Toan with name of motel

--- app/Http/Controllers/salesCtrl.php
+++ app/Http/Controllers/salesCtrl.php
@@ -3,18 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\rooms;
 use App\services;
+use App\motels;
 class salesCtrl extends Controller
 {
     //
     public function getList(){
-        $rooms = rooms::where('id_mler', 1)->get();
-//        $services = services::where(['id_mler', 1], ['id_mls',1])->get();
-        $services = services::where('id_mler', 1)->where('id_mls',1)->get();
+        $rooms = rooms::where('id_mler', (Auth::user())->id_mler)->get();
+        $services = services::where('id_mler', (Auth::user())->id_mler)->get();
+        $motels = motels::where('id_mler', (Auth::user())->id_mler)->get();
 
-        return view('moteler.sales.list',['rooms'=>$rooms, 'sers'=>$services]);
+        return view('moteler.sales.list',['rooms'=>$rooms, 'sers'=>$services, 'mls'=>$motels]);
     }
 
     public function createSales(){
--- resources/views/moteler/sales/list.blade.php
+++ resources/views/moteler/sales/list.blade.php
@@ -13,7 +13,7 @@
                 </div>
             @endif
             <?php
-                var_dump($rooms);
+                var_dump($mls);
             ?>
             <div class="table-responsive">
                 <form class="contact100-form validate-form" action="moteler/sales/create" method="POST">
@@ -33,54 +33,64 @@
                         </thead>
                         <tbody>
                         {{--one row is bill of one room--}}
-                        @foreach($rooms as $room)
-                            <tr>
-                                <td id="room_name">{{ $room->name }}</td>
-                                <td style="text-align: left">
-                                    <div>Cũ</div>
-                                    <div>Mới</div>
-                                </td>
-                                <td>
-                                    <div><input type="text" id="no_elec_old" readonly="true" value="{{ $room->no_elec }}"></div>
-                                    <div><input type="text" id="no_elec_new" value="352"></div>
-                                </td>
-                                <td style="text-align: left">
-                                    <div>Cũ</div>
-                                    <div>Mới</div>
-                                </td>
-                                <td>
-                                    <div><input type="text" id="no_water_old" readonly="true" value="{{ $room->no_water }}"></div>
-                                    <div><input type="text" id="no_water_new" value="352"></div>
-                                </td>
-                                <td>
-                                    @foreach($sers as $ser)
-                                        <div>
-                                            <input type="text" readonly="true" value="@if(in_array($ser->name, array('Điện', 'Nước'))) {{$ser->name}} @endif">
-                                        </div>
-                                    @endforeach
-                                </td>
+                        @foreach($mls as $motel)
+                            @foreach($rooms as $room)
+                                @if($motel->id == $room->id_mls)
+                                    <tr>
+                                        <td id="room_name">{{$motel->name}}-{{ $room->name }}</td>
+                                        <td style="text-align: left">
+                                            <div>Cũ</div>
+                                            <div>Mới</div>
+                                        </td>
+                                        <td>
+                                            <div><input type="text" id="no_elec_old" readonly="true" value="{{ $room->no_elec }}"></div>
+                                            <div><input type="text" id="no_elec_new" value="352"></div>
+                                        </td>
+                                        <td style="text-align: left">
+                                            <div>Cũ</div>
+                                            <div>Mới</div>
+                                        </td>
+                                        <td>
+                                            <div><input type="text" id="no_water_old" readonly="true" value="{{ $room->no_water }}"></div>
+                                            <div><input type="text" id="no_water_new" value="352"></div>
+                                        </td>
+                                        <td>
+                                            @foreach($sers as $ser)
+                                                @if($ser->id_mls == $room->id_mls)
+                                                    <div>
+                                                        <input type="text" readonly="true"
+                                                               value="@if(in_array($ser->name, array('Điện', 'Nước'))) {{$ser->name}}@endif - {{$ser->id_mls}}">
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        </td>
 
-                                <td>
-                                    @foreach($sers as $ser)
-                                        <div>
-                                            <input type="text" id="price_{{$ser->name}}" readonly="true" value="@if(in_array($ser->name, array('Điện', 'Nước'))) {{$ser->price}} @endif">
-                                        </div>
-                                    @endforeach
-                                </td>
-                                <td>...</td>Điện
-                                <td>
-                                    <div>Thanh toán</div>
-                                </td>
-                                <td>
-                                    <div><input type="text" id="pay" placeholder="Sô tiền trả"></div>
-                                    <div><input type="text" id="debt" value="" hidden="hidden"></div>
-                                </td>
-                                <td class="act">
-                                    <button class="btn btn-info">
-                                        <i class="fa fa-save" style="text-align: center; color: White;" aria-hidden="true"> <b>Lưu</b></i>
-                                    </button>
-                                </td>
-                            </tr>
+                                        <td>
+                                            @foreach($sers as $ser)
+                                                @if($ser->id_mls == $room->id_mls)
+                                                    <div>
+                                                        <input type="text" id="price_{{$ser->name}}" readonly="true"
+                                                               value="@if(in_array($ser->name, array('Điện', 'Nước'))) {{$ser->price}} @endif">
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td>...</td>Điện
+                                        <td>
+                                            <div>Thanh toán</div>
+                                        </td>
+                                        <td>
+                                            <div><input type="text" id="pay" placeholder="Sô tiền trả"></div>
+                                            <div><input type="text" id="debt" value="" hidden="hidden"></div>
+                                        </td>
+                                        <td class="act">
+                                            <button class="btn btn-info">
+                                                <i class="fa fa-save" style="text-align: center; color: White;" aria-hidden="true"> <b>Lưu</b></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @endif
+                        @endforeach
                         @endforeach
                         </tbody>
                     </table>
@@ -89,7 +99,6 @@
             <i class="fa fa-table"></i> Thanh Toán</div>
 
     </div>
-@endsection
 <style >
     tbody tr td:nth-child(3) div input, td:nth-child(5) div input, td:nth-child(6) div input, td:nth-child(7) div input, td:nth-child(10) div input{
         text-align: center;
@@ -104,8 +113,8 @@
         text-align: left;
     }
     tbody tr td:nth-child(1){
+        width: 200px;
         text-align: left;
-        margin: auto auto;
         font-weight: bold;
     }
 
